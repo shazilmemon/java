bae381e21267bc808a8b038d10ca5eeef4f2f9a6
shazilmemon <shazilmemon6@gmail.com>
2023-01-31 05:45:42 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+ 
+        // var counter = 0;
+        // function increment(){
+        //     counter++;
+        //     console.log(counter);
+        // }
+
+// const num = [0 , 1 , 2 , 3 , 4, 5 , 6, 7, 8, 9]; 
+// const data = collect(num);
+// const total = data.count();
+  
+// console.log('Total number of elements are:', {total});
+
+                                //   answer no 4
+// var count = 1;
+// var button = document.getElementById("increment");
+// var button2 =document.getElementById("decrement");
+// button.addEventListener("click", function() {
+//   var increment = document.getElementById("count");
+
+//   increment.innerText = count;
+  
+//   count++;
+// });
+                            //   answer no 5
+
+
+// function myfunction () {
+//  var row =document.getElementById("mytable")
+//  row = (mytable.remove[2]);
+// }
+
+
+
+
+
+
+
